fcc111e3a43ee99b1c8666f15f6b6a4d5911983b
Andje23 <andje23@gmail.com>
2023-10-25 18:57:50 +0300

    added method execute_query_with_params, to create database queries

--- connection.py
+++ connection.py
@@ -3,8 +3,10 @@ from PySide6 import QtWidgets, QtSql
 
 class Data:
     
+    
     def __init__(self) -> None:
         super(Data, self).__init__()
+        
         
     def create_connection(self):
         db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
@@ -19,3 +21,14 @@ class Data:
         query.exec("CREATE TABLE IF NOT EXISTS expenses (ID integer primary key AUTOINCREMENT, Dete VARCHAR(20),"
                    "Category VARCHAR(20), Description VARCHAR(20), Balance REAL, Status VARCHAR(20))")
         return True
+    
+    
+    def execute_query_with_params(self, sql_query, query_values=None):
+        query = QtSql.QSqlQuery()
+        query.prepare(sql_query)
+        
+        if query_values is not None:
+            for query_value in query_values:
+                query.addBindValue(query_value)
+        
+        query.exec()
